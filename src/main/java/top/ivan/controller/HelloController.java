858f0a233507595129acba768162e41a8f16af5d
wangyixin <wangyixin>
2017-08-15 09:26:55 +0800

    #M start a springMVC project

---
+++ src/main/java/top/ivan/controller/HelloController.java
@@ -0,0 +1,24 @@
+package top.ivan.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    private String beanName;
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+    public void setBeanName(String beanName) {
+        this.beanName = beanName;
+    }
+
+    @RequestMapping("/hello")
+    public @ResponseBody String hello() {
+        return "welcome to here by ivan!";
+    }
+}
