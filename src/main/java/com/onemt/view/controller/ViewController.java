6fb1886c6aac1acd52fb0daa21100557189984b5
wangyixin <wangyixin>
2017-08-22 08:41:31 +0800

    #M 整合数据库

--- src/main/java/com/onemt/view/controller/ViewController.java
+++ src/main/java/com/onemt/view/controller/ViewController.java
@@ -1,5 +1,6 @@
 package com.onemt.view.controller;
 
+import com.onemt.view.domain.ArticleData;
 import com.onemt.view.mapper.ArticleMapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,22 +20,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/view")
 public class ViewController {
     private Logger logger = LogManager.getLogger(ViewController.class);
-//    @Autowired
+    @Autowired
     private ArticleMapper articleMapper;
-
-    public ArticleMapper getArticleMapper() {
-        return articleMapper;
-    }
-
-    public void setArticleMapper(ArticleMapper articleMapper) {
-        logger.debug("insert mapper");
-        this.articleMapper = articleMapper;
-    }
 
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
-        logger.info("test hello");
-        return articleMapper.selectArticleById(869618391196224L).getMediaName();
+        ArticleData article =  articleMapper.selectArticleById(869618391196224L);
+        logger.info("get Article " + article.getId() + " " + article.getMediaName() + " " + article.getContentUrl());
+        return article.getMediaName();
     }
 }
